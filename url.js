57f74b32bfedf2d2435db07838616858fd7ae530
safvankottayil <117747500+safvankottayil@users.noreply.github.com>
2023-09-12 09:14:22 +0530

    port changed

--- url.js
+++ url.js
@@ -1,2 +1,2 @@
-export const userUrl='http://localhost:4000'
-export const adminUrl='http://localhost:4000/admin'
+export const userUrl='http://20.39.192.152/'
+export const adminUrl='http://20.39.192.152/admin'
