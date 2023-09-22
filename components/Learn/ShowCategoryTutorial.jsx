764e8a9df339ca526a2c90f90c27d1c74abdf605
safvankottayil <117747500+safvankottayil@users.noreply.github.com>
2023-09-22 09:47:58 +0530

    tutorial UserDeatials show

--- components/Learn/ShowCategoryTutorial.jsx
+++ components/Learn/ShowCategoryTutorial.jsx
@@ -4,6 +4,7 @@ import DocumentSearch from './DocumentSearch'
 import CreateButton from './CreateButton'
 import {userUrl} from  '@/url'
 import Link from 'next/link'
+import { MdStar } from 'react-icons/md'
 export async function generateStaticParams() {
     const res = await fetch(`${userUrl}/tutorialcategory`,{next:{revalidate:1}})
     const {category}=await res.json()
@@ -17,7 +18,7 @@ export async function generateStaticParams() {
     return res.json()
   }
 
-
+const ratingStar=[1,2,3,4,5]
 async function ShowCategoryTutorial({slag}) {
   const {Tutorials}=await GetTutorial(slag)
   return (
@@ -30,23 +31,68 @@ async function ShowCategoryTutorial({slag}) {
               </div>
             </div>
             <div className="grid grid-cols-3 pl-3 gap-5 mt-3">
-              {Tutorials.map((tuto)=>{
-                return (
-                <Link href={`/learn/tutorial/${tuto._id}/home`}>
+          {Tutorials.map((tuto) => {  
+            const reviews=tuto?.reviews?tuto.reviews.length==0?1:tuto.reviews.length:1
+            const rate=Math.floor(tuto.rating/reviews)
+            return (
+            <>
                 <div className=" shadow-md shadow-slate-700 rounded-xl">
-                  <img className='w-full h-60 rounded-t-md' src={tuto.image} alt="" />
-                  <div className='flex flex-col h-24 px-2 py-1'>
-                    <p className='font-bold capitalize'>{tuto.title}</p>
-                    <p className='overflow-y-auto capitalize'>{tuto.description}</p>
-
+                    <Link href={`/learn/tutorial/${tuto._id}/home`}>
+                  <img
+                    className="w-full h-60 rounded-t-md"
+                    src={tuto.image}
+                    alt=""
+                  />
+                     </Link>
+                  <div className="flex h-24 pr-1 py-1">
+                    <div className="px-1">
+                      <Link href={`/user/${tuto.UserId._id}`}>
+                      {tuto.UserId.image?
+                      <div  className="w-14 h-14 mt-1 rounded-full "  >
+                        <img src={tuto.UserId.image} className="w-full h-14 rounded-full" alt="" />
+                      </div>:
+                      <div className="bg-black w-14 mt-1  h-14 rounded-full"></div>}
+                      </Link>
+                    </div>
+                    <div>
+                      <div className="flex items-center justify-between">
+                        <p className="font-bold truncate capitalize pt-1">
+                          {tuto.title}
+                        </p>
+                        <p className="flex">
+                        {ratingStar.map((rating) => {
+                        if (rating <=rate) {
+                          return (
+                            <MdStar
+                              className="text-orange-300 w-6 h-6"
+                            />
+                          );
+                        } else {
+                          return (
+                            <MdStar
+                              className="text-gray-400 w-6 h-6"
+                            />
+                          );
+                        }
+                      })}
+                        </p>{" "}
+                      </div>
+                      <p
+                        style={{
+                          textOverflow: "ellipsis",
+                          whiteSpace: "normal",
+                        }}
+                        className="overflow-y-hidden capitalize text-sm  h-[60px] font-sans"
+                      >
+                        {tuto.description}
+                      </p>
+                    </div>
                   </div>
                 </div>
-                </Link>
-                )
-              })}
-             
-             
-            </div>
+           
+           </> );
+          })}
+        </div>
           </div>
         </div>
   )
